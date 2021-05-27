220841da05020f91da496de496bc74139fe1774a
Clarissa RJ Tai <clarissa0319@gmail.com>
2021-05-27 15:26:51 +0800

    Update storage_engine.py
    add basic functions and storage formats

--- storage_engine/default/storage_engine.py
+++ storage_engine/default/storage_engine.py
@@ -1,2 +1,18 @@
-def example(image):
+'''
+Storage Formats
+images/thumbnails: .png
+metadata: .csv for small size, .json for hierarchial structure
+features: saved as .npy or pickle
+
+'''
+def dump_one(image):
+    pass
+
+def dump_many(image):
+    pass
+
+def read_one(image):
+    pass
+
+def read_many(image):
     pass
