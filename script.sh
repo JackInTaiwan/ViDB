af86f77233d7f0481f16e3644b56532af2c0602a
jackintaiwan <jackconvolution@gmail.com>
2021-06-08 13:20:25 +0800

    update: Add all operation test cases into script.sh.

--- script.sh
+++ script.sh
@@ -2,5 +2,30 @@
 python3 -m cli.server
 
 
-# Run a client test case
-python3 -m test_case.client_example
+# Run client test cases
+python3 -m test_case.client_insert -o insert_one_by_path
+python3 -m test_case.client_insert -o insert_many_by_dir
+python3 -m test_case.client_insert -o insert_one_by_byte
+python3 -m test_case.client_insert -o insert_many_by_byte
+
+python3 -m test_case.client_update -o update_one_by_id
+python3 -m test_case.client_update -o update_many_by_ids
+
+python3 -m test_case.client_browse -o browse_by_random
+python3 -m test_case.client_browse -o browse_by_cluster
+
+python3 -m test_case.client_retrieve -o retrieve_one
+python3 -m test_case.client_retrieve -o retrieve_many
+
+python3 -m test_case.client_query -o query_nearest_by_content
+python3 -m test_case.client_query -o query_nearest_by_style
+python3 -m test_case.client_query -o query_farthest_by_content
+python3 -m test_case.client_query -o query_farthest_by_style
+python3 -m test_case.client_query -o query_by_tag_all
+python3 -m test_case.client_query -o query_by_tag_partial
+python3 -m test_case.client_query -o query_range_by_content
+python3 -m test_case.client_query -o query_range_by_style
+
+python3 -m test_case.client_delete -o delete_one_by_id
+python3 -m test_case.client_delete -o delete_many_by_ids
+python3 -m test_case.client_delete -o delete_all_data
